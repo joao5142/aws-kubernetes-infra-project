b1746e6e544b79b985cc0242ae60c3577d55583a
joao5142 <joaopauloneto3687@gmail.com>
2026-03-15 19:04:03 -0300

    fix: adjust networking stack

--- 001-networking/02-8-vpc.private-route-tables.tf
+++ 001-networking/02-8-vpc.private-route-tables.tf
@@ -5,7 +5,7 @@ resource "aws_route_table" "private_route_table" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.this.id
+    nat_gateway_id = aws_nat_gateway.this.id
   }
 }
 
